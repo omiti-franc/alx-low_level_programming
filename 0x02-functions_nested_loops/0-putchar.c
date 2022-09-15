47add20cb20558c238caae4eba1417452830b0c1
omiti-franc <omitifranc@gmail.com>
2022-09-15 06:12:00 -0700

    Latest commits

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,16 +6,13 @@
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	
-	return (0)
+	char *_putchar = 'Putchar';
 
+	      while (*_putchar);
+	      {
+		      _putchar(*_putchar);
+		      _putchar++;
+	      }
+	      _putchar('\n');
+	      return (0)
 }
