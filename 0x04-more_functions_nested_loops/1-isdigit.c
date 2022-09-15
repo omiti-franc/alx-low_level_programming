39116c06e50327a8696c141143670556eac8878c
omiti-franc <omitifranc@gmail.com>
2022-09-15 08:04:12 -0700

    isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,12 +2,12 @@
 
 /**
  *_isdigit - checks for digithood
- * @c: character to checked for digit membership
- * Return: 1 if digit, 0 if not digit
+ *@c: character to checked for digit membership
+ *Return: 1 if digit, 0 if not digit
  */
 int _isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
-		return (1)
-	return (0)
+		return (1);
+	return (0);
 }
