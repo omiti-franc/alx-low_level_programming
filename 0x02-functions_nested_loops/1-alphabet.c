a51ed2b60d9bb6c5d7da88c2e7caa44a07bec5d5
omiti-franc <112470574+omiti-franc@users.noreply.github.com>
2022-09-16 10:26:05 +0300

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,6 +4,7 @@
  *print_alphabet - function to print abc
  *Return: 0
  */
+
 void print_alphabet(void)
 {
 	char c;
@@ -12,5 +13,5 @@ void print_alphabet(void)
 	{
 		_putchar(c);
 	}
-_putchar('\n');
+        _putchar('\n');
 }
