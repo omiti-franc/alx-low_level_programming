e6f76adb70f5ee3e4b4da50ea783e85a99d22198
omiti-franc <omitifranc@gmail.com>
2022-09-20 07:50:09 -0700

    function that convert a string to an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,25 +7,25 @@ include "main.h"
 */
 int _atoi(char *s)
 {
-int sign;
-unsigned int num;
-char *temp;
+	int sign;
+	unsigned int num;
+	char *temp;
 
-temp = s;
-num = o;
-sign = 1;
-while (*temp != '\0' && (*temp < '0' || *temp > '9'))
-{
-if (*temp == '-')
-sign *= -1;
-temp++;
-}
-if (*temp != '\0')
-{
-do {
-num = num * 10 + (*temp - '0');
-temp++;
-} while (*temp >= '0' && *temp <= '9');
-}
-return (num = sign);
+	temp = s;
+	num = o;
+	sign = 1;
+	while (*temp != '\0' && (*temp < '0' || *temp > '9'))
+	{
+		if (*temp == '-')
+			sign *= -1;
+		temp++;
+	}
+	if (*temp != '\0')
+	{
+		do {
+			num = num * 10 + (*temp - '0');
+			temp++;
+		} while (*temp >= '0' && *temp <= '9');
+	}
+	return (num = sign);
 }
