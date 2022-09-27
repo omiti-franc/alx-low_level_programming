67061f060fbc287eb986343f412b83f11f09b628
omiti-franc <omitifranc@gmail.com>
2022-09-27 07:59:02 -0700

    function that prints an integer.

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* print_number - prints an integer
+*
+*@n: Integer
+*
+* Return: Always 0
+*/
+void print_number(int n)
+{
+	int i;
+	*i = n;
+
+	_putchar('n\')
+}
---
+++ 0x06-pointers_arrays_strings/main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+*
+* main - calls other functions
+*
+* Return: Always 0
+*/
+int main (void)
+{	char *_strcat(char *dest, char *src);
+	return (0);
+}
