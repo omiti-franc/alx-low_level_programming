fd98eb2926a4e73eadf1c6784616f3b4d06e2ef0
omiti-franc <112470574+omiti-franc@users.noreply.github.com>
2022-09-28 12:28:35 +0300

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,9 +9,15 @@
 */
 void _puts_recursion(char *s)
 {
-	_puts_recursion(*s)
-
-	_putchar ('\n');
-
-	return (0);
+	
+	if (*s != '\0')
+	{
+		_putchar(*(s + 0));
+		_puts_recursion((s + 1));
+	}
+	else
+	{
+		_putchar('\n');
+		return;
+	}
 }
