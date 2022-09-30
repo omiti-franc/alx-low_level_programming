8ebeeacb0566e860cb84418b18f78749f65feb34
omiti-franc <omitifranc@gmail.com>
2022-09-30 03:22:50 -0700

    program should print a number, followed by a new line

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include Â"main.h"
+#include <stdio.h>
+
+/**
+* main - Prints number of arguments
+* @argc: Arg counts
+* @argv: Arg vector
+*
+* Return: Always 0
+*/
+int main(int argc, char *argv[])
+
+{
+
+	(void) argv;
+
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
