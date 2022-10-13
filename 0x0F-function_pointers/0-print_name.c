a8bff92bfdefd7d8446194a11e6794d9b3f70081
omiti-franc <omitifranc@gmail.com>
2022-10-13 05:56:43 -0700

    a function that prints a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+/**
+* print_name - function that prints a name
+*
+* @name: string
+*
+* @f: function pointer
+*
+* Return: void
+*/
+void print_name(char *name, void (*f)(char *));
+{
+	if (f == NULL)
+		return;
+	f(name);
+}
