ff1d207b9a4df979ee9f904abb1d824e6370f745
Francis Omiti <112470574+omiti-franc@users.noreply.github.com>
2022-10-27 12:34:03 +0300

    Update lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -5,20 +5,19 @@
 #include <string.h>
 
 /**
-* struct list_s - singly linked list
-* @n: Integer
-* @next: points to the next node
-*
-* Description: singly linked list node structure
-*/
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
 
-typedef struct list_s
+typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
-int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -32,6 +31,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
-size_t free_listint_safe(listint_t **h);
+
 listint_t *find_listint_loop(listint_t *head);
 #endif
