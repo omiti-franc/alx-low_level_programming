5fde60be029d4a48f113d70814f4b183e891ab24
Francis Omiti <112470574+omiti-franc@users.noreply.github.com>
2022-12-08 18:31:40 +0300

    Update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,77 +1,55 @@
 #include "lists.h"
 
-
 /**
- * dlistint_len - returns the number of elements in a dlistint_t list
- * @h: head of doubly linked list
+ * insert_dnodeint_at_index -  inserts a new node
+ * at a given position.
  *
- * Return: number of nodes
+ * @head: pointer to the head
+ * @n: data to add
+ * @idx: position to insert
+ * Return: pointer to new node
  */
 
-size_t dlistint_len(const dlistint_t *h)
+dlistint_t *insert_dnodeint_at_index(
+		dlistint_t **head, unsigned int idx, int n)
 {
-	int count = 0;
-
-	while (h)
-	{
-		count++;
-		h = h->next;
-	}
-	return (count);
-}
-
-
-
-/**
- * insert_dnodeint_at_index - inserts a new node at a given position
- * @h: head of linked list
- * @idx: index
- * @n: integer value of node
- *
- * Return: address of new node, return NULL if fails
- */
-
-
-dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
-{
-	dlistint_t *new, *temp;
-	size_t length;
+	dlistint_t *new, *h;
 	unsigned int i = 0;
 
-	if (h == NULL)
-		return (NULL);
-	if (idx == 0)
-		return (add_dnodeint(h, n));
-
-	length = dlistint_len(*h);
-	if (idx == length - 1)
-		return (add_dnodeint_end(h, n));
-
+	if (!head)
+		return (0);
 	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
-		return (NULL);
+	if (!new)
+		return (0);
 	new->n = n;
-	if (*h == NULL)
+	if (!(*head) && idx == 0)
 	{
-		new->prev = NULL;
-		new->next = NULL;
-		*h = new;
+		*head = new;
 		return (new);
 	}
-	temp = *h;
-	while (temp)
+	h = *head;
+	while (h->next && i < idx)
 	{
-		if (i == idx)
-		{
-			new->next = temp;
-			new->prev = temp->prev;
-			temp->prev->next = new;
-			temp->prev = new;
-			return (new);
-		}
-		temp = temp->next;
+		h = h->next;
 		i++;
 	}
-	free(new);
-	return (NULL);
+	if (i == idx)
+	{
+		new->next = h;
+		if (h->prev)
+			h->prev->next = new;
+		else
+			*head = new;
+		new->prev = h->prev;
+		h->prev = new;
+	} else if (idx == i + 1)
+	{
+		h->next = new;
+		new->prev = h;
+	} else
+	{
+		free(new);
+		return (0);
+	}
+	return (new);
 }
